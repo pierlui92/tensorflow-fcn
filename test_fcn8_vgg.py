c993adcfddb57e3b750b7591986069d5297ab26a
Pierluigi Zama Ramirez <zamapierluigi@hotmail.it>
2018-06-24 16:45:25 +0200

    update

--- test_fcn8_vgg.py
+++ test_fcn8_vgg.py
@@ -12,39 +12,66 @@ import sys
 import fcn8_vgg
 import utils
 
+batch_size=8
+
+def build_input(input_list_txt="/home/pier/pier_data/tensorflow-fcn/input_list_train_cityscapes.txt"):
+    def _parse_function(image_tensor):
+        image = tf.read_file(image_tensor[0])
+        image_sem = tf.read_file(image_tensor[1])
+        image = tf.image.decode_image(image, channels = 3)
+        image_sem = tf.image.decode_image( image_sem , channels = 1)
+        image.set_shape([None,None,3])
+        image_sem.set_shape([None,None,1])
+        return image , image_tensor[0], image_sem
+        
+    samples=[]
+    samples_sem = []
+    
+    with open(input_list_txt, "r") as input_list:
+        for line in input_list:
+            sample,sample_sem = line.strip().split(";")
+            samples.append(sample.strip())
+            samples_sem.append(sample_sem.strip())
+
+    inputs = np.stack((samples, samples_sem), axis = -1)
+    
+    image_tensor = tf.constant(inputs, tf.string)
+    dataset = tf.data.Dataset.from_tensor_slices(image_tensor)
+    dataset = dataset.map(_parse_function)
+    dataset = dataset.repeat()
+    iterator = dataset.make_one_shot_iterator()
+    return image , image_path, image_sem = iterator.get_next()
+
+
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
                     level=logging.INFO,
                     stream=sys.stdout)
 
 from tensorflow.python.framework import ops
 
-img1 = scp.misc.imread("./test_data/tabby_cat.png")
 
 with tf.Session() as sess:
-    images = tf.placeholder("float")
-    feed_dict = {images: img1}
-    batch_images = tf.expand_dims(images, 0)
-
-    vgg_fcn = fcn8_vgg.FCN8VGG()
+    immy_a,_,immy_a_sem= build_input_image_op(input_list_train)
+    input_images,input_sem_gt = tf.train.shuffle_batch([immy_a,immy_a_sem],batch_size,100,30,8)
     with tf.name_scope("content_vgg"):
-        vgg_fcn.build(batch_images, debug=True)
+        vgg_fcn = fcn8_vgg.FCN8VGG()
+        vgg_fcn.build(input_images, debug=True)
+        print('Finished building Network.')
 
-    print('Finished building Network.')
-
-    logging.warning("Score weights are initialized random.")
-    logging.warning("Do not expect meaningful results.")
-
-    logging.info("Start Initializing Variabels.")
-
-    init = tf.global_variables_initializer()
-    sess.run(init)
+    # init = [tf.global_variables_initializer(),tf.local_variables_initializer()]
+    # sess.run(init)
+        
+    coord = tf.train.Coordinator()
+    tf.train.start_queue_runners()
+    print('Thread running')
 
     print('Running the Network')
-    tensors = [vgg_fcn.pred, vgg_fcn.pred_up]
-    down, up = sess.run(tensors, feed_dict=feed_dict)
+    tensors = [vgg_fcn.pred_up,vgg_fcn.upscore32]
+    up, score = sess.run(tensors)
+    print(score.shape)
+    
+    coord.request_stop()
+    coord.join(stop_grace_period_secs=10)
 
-    down_color = utils.color_image(down[0])
     up_color = utils.color_image(up[0])
-
-    scp.misc.imsave('fcn8_downsampled.png', down_color)
     scp.misc.imsave('fcn8_upsampled.png', up_color)
